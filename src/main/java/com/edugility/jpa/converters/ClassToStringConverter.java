68bd18e81e7245cd274f4f9689830c929c57e865
Laird Nelson <ljnelson@gmail.com>
2014-05-30 16:48:38 -0700

    Initial revision

---
+++ src/main/java/com/edugility/jpa/converters/ClassToStringConverter.java
@@ -0,0 +1,58 @@
+/* -*- mode: Java; c-basic-offset: 2; indent-tabs-mode: nil; coding: utf-8-unix -*-
+ *
+ * Copyright (c) 2014 Edugility LLC.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * The original copy of this license is available at
+ * http://www.opensource.org/license/mit-license.html.
+ */
+package com.edugility.jpa.converters;
+
+import javax.persistence.AttributeConverter;
+
+public class ClassToStringConverter implements AttributeConverter<Class<?>, String> {
+
+  public ClassToStringConverter() {
+    super();
+  }
+
+  @Override
+  public String convertToDatabaseColumn(final Class<?> cls) {
+    if (cls == null) {
+      return null;
+    }
+    return cls.getName();
+  }
+
+  @Override
+  public Class<?> convertToEntityAttribute(final String className) {
+    if (className == null) {
+      return null;
+    }
+    try {
+      return Class.forName(className, true, Thread.currentThread().getContextClassLoader());
+    } catch (final ClassNotFoundException cnfe) {
+      throw new IllegalArgumentException(className, cnfe);
+    }
+  }
+
+}
